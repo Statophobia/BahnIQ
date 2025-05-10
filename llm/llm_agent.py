b0f658a94f921a058b94b0911bca44c185fd3012
Subramanian Veerappan <subru1997@gmail.com>
2025-05-10 20:20:20 +0200

    add groq_api_key env

--- llm/llm_agent.py
+++ llm/llm_agent.py
@@ -3,16 +3,19 @@ from langchain.agents import create_sql_agent
 from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from langchain.sql_database import SQLDatabase
 
+import os
+
 from pathlib import Path
 
 db_path = Path('data')/'db_data.db'
+GROQ_API_KEY = os.environ.get('GROQ_API_KEY')
 
 def get_sql_agent():
     db = SQLDatabase.from_uri("duckdb:///" + str(db_path.resolve()))
 
     llm = ChatGroq(
         model_name="gemma2-9b-it",
-        api_key="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"  # Replace with your actual key or load from env
+        api_key=GROQ_API_KEY
     )
 
     toolkit = SQLDatabaseToolkit(db=db, llm=llm)
